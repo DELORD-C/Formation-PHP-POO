c13a581162adc8636076ff9fdf230c581cc6be1b
Clément <clement.delord@epitech.eu>
2021-11-19 09:08:45 +0100

    start style ex 10

--- Exercices/Exercice 10/index.php
+++ Exercices/Exercice 10/index.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Recherche</title>
 </head>
 <body>
-    <form action="recherche.php" method="post">
-        <input name='search' type="text" placeholder="Recherche">
-        <input type="submit" value="Rechercher">
+    <form action="recherche.php" method="post" class='recherche'>
+        <input name='search' type="text" placeholder="Recherche" id='search'>
+        <input type="submit" value="Rechercher" id='submit-search'>
     </form>
-    <form action="ajout.php" method="post">
+    <form action="ajout.php" method="post" class='creation'>
         <input type="text" name='titre' placeholder="Titre">
         <input type="text" name='realisateurs' placeholder="Realisateur(s)">
         <input type="text" name='sections' placeholder="Sections">
@@ -22,7 +23,7 @@
     <?php if (!empty($_GET['info'])) { ?>
         <span><?=$_GET['info']?></span>
     <?php } ?>
-    <div>
+    <div class='liste'>
         <?php
             include('class/Films.php');
             $films = new Films ();
